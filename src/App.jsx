320fbfd08a0dedcfd0eea7060dde8cfee0615b4c
Harneet <harneetsahi8@gmail.com>
2025-01-08 20:24:12 -0600

    counter function with some conditions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
 
 function App() {
-  const username = "Harneet";
+  const [counter, setCounter] = useState(0);
+
+  const addCount = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+
+  const removeCount = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <>
       <h1>Chai aur Code</h1>
-      <p>{username}</p>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addCount}>Add count</button>
+
+      <button onClick={removeCount}>Remove count</button>
     </>
   );
 }
